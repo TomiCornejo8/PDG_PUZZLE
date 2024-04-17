5a43dd6b31280c1e8bc1d1beb257fda73cbdae28
Nicolas bobadilla sanchez <nicolas.bobadilla.s@mail.pucv.cl>
2024-04-17 18:55:07 -0400

    Se agregan colores a la matriz

---
+++ auxiliar/colorRender.py
@@ -0,0 +1,41 @@
+
+import pandas as pd
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
+
+
+
+
+def html_to_argb(html_color):
+    # Reorganizar el código de color HTML para que sea compatible con openpyxl (RRGGBB a AABBGGRR)
+    hex_color = html_color[1:]
+    argb_color = 'FF' + hex_color[4:] + hex_color[2:4] + hex_color[:2]
+    return argb_color
+
+def renderMatrix(map):
+    wb = Workbook()
+    hoja = wb.active
+    for fila_idx, fila in enumerate(map, start=1):
+        for celda_idx, valor_celda in enumerate(fila, start=1):
+            hoja.cell(row=fila_idx, column=celda_idx, value=valor_celda)
+            if valor_celda == 0:
+                relleno = PatternFill(start_color=html_to_argb("#f0f0f0"), end_color=html_to_argb("#f0f0f0"), fill_type="solid")  # Relleno amarillo
+                hoja.cell(row=fila_idx, column=celda_idx).fill = relleno
+            if valor_celda == 1:
+                relleno = PatternFill(start_color=html_to_argb("#c0c0c0"), end_color=html_to_argb("#c0c0c0"), fill_type="solid")  # Relleno amarillo
+                hoja.cell(row=fila_idx, column=celda_idx).fill = relleno
+            if valor_celda == 2:
+                relleno = PatternFill(start_color=html_to_argb("#ffb6c1"), end_color=html_to_argb("#ffb6c1"), fill_type="solid")  # Relleno amarillo
+                hoja.cell(row=fila_idx, column=celda_idx).fill = relleno  
+            if valor_celda == 3:
+                relleno = PatternFill(start_color=html_to_argb("#d2b48c"), end_color=html_to_argb("#d2b48c"), fill_type="solid")  # Relleno amarillo
+                hoja.cell(row=fila_idx, column=celda_idx).fill = relleno
+            if valor_celda == 4:
+                relleno = PatternFill(start_color=html_to_argb("#98fb98"), end_color=html_to_argb("#98fb98"), fill_type="solid")  # Relleno amarillo
+                hoja.cell(row=fila_idx, column=celda_idx).fill = relleno
+            if valor_celda == 5:
+                relleno = PatternFill(start_color=html_to_argb("#add8e6"), end_color=html_to_argb("#add8e6"), fill_type="solid")  # Relleno amarillo
+                hoja.cell(row=fila_idx, column=celda_idx).fill = relleno
+
+    # Guardar los cambios en el archivo Excel
+    wb.save("matriz_con_celda_pintada.xlsx")
--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import time as T
 
 # Imports propios
 from modules import ScenarioFiller as SF
+from auxiliar import colorRender as color
 
 # MAIN
 inicio = T.time()
 print(f"Se inicia la generación del mapa")
 
-map = SF.scenarioFiller(100,100,0.1)
+map = SF.scenarioFiller(100,100,0.5)
 
 voidSpace = np.argwhere(map == 0)
 player = voidSpace[np.random.choice(len(voidSpace))]
@@ -18,3 +19,6 @@ map[player[0],player[1]] = 5
 np.savetxt('result.csv', map, delimiter=',', fmt='%d')
 
 print(f"Mapa <result.csv> generado en {T.time() - inicio} segundos")
+
+color.renderMatrix(map)
+
