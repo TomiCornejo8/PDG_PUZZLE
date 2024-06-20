b5f9188c54a00145929af1473023d7135e8e92cf
TomiCornejo8 <tomas.cornejo.z@mail.pucv.cl>
2024-06-20 17:22:36 -0400

    Nuevo solver

--- modules/mechanics.py
+++ modules/mechanics.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Moves
 RIGHT = (0,1)
 LEFT = (0,-1)
@@ -7,6 +9,7 @@ STAY = (0,0)
 
 MOVES = [RIGHT,LEFT,UP,DOWN]
 
+# Entitys
 EMPTY = 0
 WALL = 1
 ENEMY = 3
@@ -15,38 +18,61 @@ DOOR = 4
 PLAYER = 5
 
 # Functions
-def lookAhead(player,move):
-    return [player[0]+move[0],player[1]+move[1]]
+def getPlayer(dungeon):
+    return np.where(dungeon == PLAYER)
 
-def iceSliding(dungeon,player,move):
-    dungeon[player[0],player[1]] = EMPTY
+def lookAhead(dungeon,move):
+    player = getPlayer(dungeon)
+    tile = [player[0]+move[0],player[1]+move[1]]
+    entity = dungeon[tile[0],tile[1]]
+    return tile,entity
 
-    while dungeon[lookAhead(player,move)[0],lookAhead(player,move)[1]] == EMPTY: 
-        player = lookAhead(player,move)
-        
-    dungeon[player[0],player[1]] = PLAYER
-
+def iceSliding(dungeon,move):
+    while True: 
+        tile,entity = lookAhead(dungeon,move)
+        if entity == EMPTY:
+            player = getPlayer(dungeon)
+            dungeon[player[0],player[1]] = EMPTY
+            dungeon[tile[0],tile[1]] = PLAYER
+        else:
+            break
     return dungeon
 
 def killEnemy(dungeon,enemy):
     dungeon[enemy[0],enemy[1]] = EMPTY
     return dungeon
 
-def getAllowMoves(dungeon,player):
-    notAllow = [WALL,BLOCK,ENEMY,DOOR]
-    newMoves = []
-    for move in MOVES:
-        tile = lookAhead(player,move)
-        dungeonTile = dungeon[tile[0],tile[1]]
-        if not dungeonTile in notAllow:
-            newMoves.append(move)
+def step(dungeon,move):
+    tile,entity = lookAhead(dungeon,move)
+    if entity == EMPTY:
+        dungeon = iceSliding(dungeon,move)
+    elif entity == ENEMY:
+        dungeon = killEnemy(dungeon,tile)
+    return dungeon
 
-    return newMoves
-
-def getMeleeEnemys(dungeon,player):
-    enemys = []
+def win(dungeon):
+    lenEnemys = len(np.argwhere(dungeon == ENEMY))
+    if lenEnemys > 0: return False
+    
     for move in MOVES:
-        tile = lookAhead(player,move)
-        if dungeon[tile[0],tile[1]] == ENEMY:
-            enemys.append(tile)
-    return enemys
+        _,entity = lookAhead(dungeon,move)
+        if entity == DOOR: return True
+
+    return False
+
+def getMove(move):
+    if move == RIGHT: return "RIGHT"
+    elif move == LEFT: return "LEFT"
+    elif move == UP: return "UP"
+    else: return "DOWN"
+
+def stepWithMoves(dungeon,move):
+    text = ""
+    tile,entity = lookAhead(dungeon,move)
+    if entity == EMPTY:
+        dungeon = iceSliding(dungeon,move)
+        text = f"Move {getMove(move)}"
+    elif entity == ENEMY:
+        dungeon = killEnemy(dungeon,tile)
+        text = f"Kill {getMove(move)}"
+    return dungeon,text
--- modules/solver.py
+++ modules/solver.py
@@ -7,84 +7,48 @@ import copy
 # Internal
 from modules import mechanics as M
 
-def getNEnemys(dungeon):
-    return np.count_nonzero(dungeon == M.ENEMY)
-
-def getEnemys(dungeon):
-    return np.where(dungeon == M.ENEMY)
-
-def getPlayer(dungeon):
-    return np.where(dungeon == M.PLAYER)
-
-def inDoor(player,dungeon):
-    for move in M.MOVES:
-        tile = M.lookAhead(player,move)
-        if dungeon[tile[0],tile[1]] == M.DOOR:
-            return True
-    return False
-
-def getDungeonHash(dungeon):
+def hashDungeon(dungeon):
     return hash(tuple(map(tuple, dungeon)))
 
-def setRoutesTree(routes,memoryStates,newDungeon,currentNode):
-    dungeonHash = getDungeonHash(newDungeon)
-    if not dungeonHash in memoryStates:
-        memoryStates.append(dungeonHash)
-        routes.append(Node(newDungeon,parent=currentNode))  
-    return routes,memoryStates
+def getRoute(leaf,parent):
+    node = Node(leaf,parent=parent)
+    route = deque()
+    while node is not None:
+        route.append(node.name)
+        node = node.parent
+    return route
 
-def getRoutes(nodo):
-    ruta = deque()
-    while nodo is not None:
-        ruta.append(nodo.name)
-        nodo = nodo.parent
-    return ruta
+def setState(states,memory,dungeon,parent):
+    hashD = hashDungeon(dungeon)
+    if not hashD in memory:
+        memory.append(hashD)
+        states.append(Node(dungeon,parent=parent))  
+    return states,memory
 
-def setRoutes(routes,memoryStates,newDungeon):
-    dungeonHash = getDungeonHash(newDungeon)
-    if not dungeonHash in memoryStates:
-        memoryStates.append(dungeonHash)
-        routes.append(newDungeon)  
-    return routes,memoryStates
-
-def playable(dungeon):
-    routes = deque()
-    memoryStates = []
-
-    routes.append(dungeon)
-    memoryStates.append(getDungeonHash(dungeon))
-      
-    while routes:
-        currentDungeon = routes.popleft()
-        player = getPlayer(currentDungeon)
+def solveGame(dungeon):
+    states = deque()
+    states.append(Node(dungeon.copy()))
+    
+    memory = []
+    memory.append(hashDungeon(dungeon))
+    
+    solutions = []
         
-        moves = M.getAllowMoves(currentDungeon,player)
-        enemys = M.getMeleeEnemys(currentDungeon,player)
-        
-        for enemy in enemys:
+    while states:
+        currentState = states.popleft()
+        currentDungeon = currentState.name
+
+        for move in M.MOVES:
             newDungeon = currentDungeon.copy()
-            newDungeon = M.killEnemy(newDungeon,enemy)
-
-            if getNEnemys(newDungeon) == 0 and inDoor(player,newDungeon): 
-                return True
+            newDungeon = M.step(newDungeon,move)
+            if M.win(newDungeon):
+                solutions.append(getRoute(newDungeon,currentState))
             else:
-                routes,memoryStates = setRoutes(routes,memoryStates,newDungeon)
-
-        for move in moves:
-            newDungeon = currentDungeon.copy()
-
-            newDungeon = M.iceSliding(newDungeon,player,move)
-
-            newPlayer = getPlayer(newDungeon)
-            
-            if getNEnemys(newDungeon) == 0 and inDoor(newPlayer,newDungeon): 
-                  return True
-            else:
-                routes,memoryStates = setRoutes(routes,memoryStates,newDungeon)
-    return False
+                states,memory = setState(states,memory,newDungeon,currentState)
+    return solutions
 
 def nSolutions(dungeon):
-    solutions = solve(dungeon)
+    solutions = solveGame(dungeon)
     
     n = len(solutions)
     if n==0: return n,0
@@ -95,108 +59,40 @@ def nSolutions(dungeon):
         if m < minM:
             minM = m
     return n,minM
-    
-def solve(dungeon):
-    routes = deque()
-    memoryStates = []
-
-    root = Node(dungeon.copy())
-    routes.append(root)
-    memoryStates.append(getDungeonHash(dungeon))
-    solutions = []
-        
-    while routes:
-        currentNode = routes.popleft()
-        currentDungeon = currentNode.name
-        player = getPlayer(currentDungeon)
-        
-        moves = M.getAllowMoves(currentDungeon,player)
-        enemys = M.getMeleeEnemys(currentDungeon,player)
-
-        for enemy in enemys:
-            newDungeon = currentDungeon.copy()
-            
-            newDungeon = M.killEnemy(newDungeon,enemy)
-
-            if getNEnemys(newDungeon) == 0 and inDoor(player,newDungeon): 
-                solutions.append(getRoutes(Node(newDungeon,parent=currentNode)))
-            else:
-                routes,memoryStates = setRoutesTree(routes,memoryStates,newDungeon,currentNode)
-
-        for move in moves:
-            newDungeon = currentDungeon.copy()
-
-            newDungeon = M.iceSliding(newDungeon,player,move)
-            newPlayer  = getPlayer(newDungeon)
-            
-            if getNEnemys(newDungeon) == 0 and inDoor(newPlayer,newDungeon): 
-                solutions.append(getRoutes(Node(newDungeon,parent=currentNode)))
-            else:
-                routes,memoryStates = setRoutesTree(routes,memoryStates,newDungeon,currentNode)
-    return solutions
 
 class DungeonWMoves:
     def __init__(self,dungeon,movement):
         self.dungeon = dungeon
         self.move = movement
 
-def setRoutesTreeWMoves(routes,memoryStates,newDungeon,currentNode):
-    dungeonHash = getDungeonHash(newDungeon.dungeon)
-    if not dungeonHash in memoryStates:
-        memoryStates.append(dungeonHash)
-        routes.append(Node(newDungeon,parent=currentNode))  
-    return routes,memoryStates
+def setStateWithMoves(states,memory,dungeon,parent):
+    hashD = hashDungeon(dungeon.dungeon)
+    if not hashD in memory:
+        memory.append(hashD)
+        states.append(Node(dungeon,parent=parent))  
+    return states,memory
 
-def getMove(move):
-    if move == M.RIGHT: return "RIGHT"
-    elif move == M.LEFT: return "LEFT"
-    elif move == M.UP: return "UP"
-    else: return "DOWN"
-
-def getKill(player,move):
-    playerInt=[int(player[0]),int(player[1])]
-    moveInt=[int(move[0]),int(move[1])]    
-    movement = [moveInt[0] - playerInt[0],moveInt[1] - playerInt[1]]
-    if movement == [0,1]: return "RIGHT"
-    elif movement == [0,-1]: return "LEFT"
-    elif movement == [-1,0]: return "UP"
-    else: return "DOWN"
-
-def solverWithMoves(dungeon):
-    routes = deque()
-    memoryStates = []
-    dungeonWMoves = DungeonWMoves(dungeon,"")
-    root = Node(dungeonWMoves)
-    routes.append(root)
-    memoryStates.append(getDungeonHash(dungeonWMoves.dungeon))
-
+def solveGameWithMoves(dungeon):
+    states = deque()
+    dungeonWMoves = DungeonWMoves(dungeon.copy(),"")
+    states.append(Node(dungeonWMoves))
+    
+    memory = []
+    memory.append(hashDungeon(dungeon))
+    
     solutions = []
         
-    while routes:
-        currentNode = routes.popleft()
-        currentDungeon = currentNode.name
-        player = getPlayer(currentDungeon.dungeon)
-        
-        moves = M.getAllowMoves(currentDungeon.dungeon,player)
-        enemys = M.getMeleeEnemys(currentDungeon.dungeon,player)
+    while states:
+        currentState = states.popleft()
+        currentDungeon = currentState.name
 
-        for enemy in enemys:
-            newDungeon = copy.deepcopy(currentDungeon)
-            newDungeon = DungeonWMoves(M.killEnemy(newDungeon.dungeon,enemy),"Kill " + getKill(player,enemy))
+        for move in M.MOVES:
+            newDungeon = copy.deepcopy(currentDungeon.dungeon)
+            newDungeon,movement = M.stepWithMoves(newDungeon,move)
+            newDungeonM = DungeonWMoves(newDungeon,movement)
 
-            if getNEnemys(newDungeon.dungeon) == 0 and inDoor(player,newDungeon.dungeon): 
-                solutions.append(getRoutes(Node(newDungeon,parent=currentNode)))
+            if M.win(newDungeon):
+                solutions.append(getRoute(newDungeonM,currentState))
             else:
-                routes,memoryStates = setRoutesTreeWMoves(routes,memoryStates,newDungeon,currentNode)
-
-        for move in moves:
-            newDungeon = copy.deepcopy(currentDungeon)
-
-            newDungeon = M.iceSliding(newDungeon.dungeon,player,move)
-            newPlayer  = getPlayer(newDungeon)
-            newDungeon =  DungeonWMoves(newDungeon,"Move "+ getMove(move))
-            if getNEnemys(newDungeon.dungeon) == 0 and inDoor(newPlayer,newDungeon.dungeon): 
-                solutions.append(getRoutes(Node(newDungeon,parent=currentNode)))
-            else:
-                routes,memoryStates = setRoutesTreeWMoves(routes,memoryStates,newDungeon,currentNode)
-    return solutions  
+                states,memory = setStateWithMoves(states,memory,newDungeonM,currentState)
+    return solutions
